58d717707d2845f642b342d38194a4fe8cac24a9
Jason Kridner <jdk@ti.com>
2013-02-28 18:25:50 +0000

    ec2-build: added some error handling and detect running instance

--- ec2-build/build-kernel.js
+++ ec2-build/build-kernel.js
@@ -16,9 +16,41 @@ var instanceConfig = {
  }
 };
 
-ec2build.run(instanceConfig, onRun);
+try {
+ var instance = ec2build.run(instanceConfig, onRun);
+ instance.on('error', onError);
+} catch(ex) {
+ console.log('ERROR: ' + ex);
+ ec2build.stop();
+}
 
-function onRun(err, data) {
+function onError(err) {
+ console.log("Error starting build");
  console.log("err = " + err);
- console.log("data = " + JSON.stringify(data));
+ ec2build.stop();
 };
+
+var address = null;
+function onRun(err, data) {
+ console.log("Build running");
+ console.log("err = " + err);
+ console.log("name = " + data.name);
+ console.log("address = " + data.address);
+ console.log("data = " + JSON.stringify(data));
+ if(err) {
+  ec2build.stop();
+ } else {
+  // start checking status after a minute
+  setTimeout(checkStatus, 60000);
+ }
+};
+
+var timesChecked = 0;
+function checkStatus() {
+ // stop it after 15 minutes for now
+ timesChecked++;
+ if(timesChecked > 15) {
+  ec2build.stop();
+ }
+ setTimeout(checkStatus, 60000);
+}
--- ec2-build/ec2-build.js
+++ ec2-build/ec2-build.js
@@ -1,61 +1,125 @@
 var AWS = require('aws-sdk');
 var config = require('./config');
+var events = require('events');
+
+var instanceId = null;
 
 function run(instanceConfig, runCallback) {
- AWS.config.update(config);
- var ec2 = new AWS.EC2.Client({region:'us-east-1'});
+ var emitter = new events.EventEmitter;
+ try {
+  AWS.config.update(config);
+  var ec2 = new AWS.EC2.Client({region:'us-east-1'});
 
- ec2.requestSpotInstances(instanceConfig, hRequestSpotInstances);
+  ec2.requestSpotInstances(instanceConfig, hRequestSpotInstances);
+ } catch(ex2) {
+  emitter.emit('error', ex2);
+ }
 
- function hRequestSpotInstances(err, data) {
-  console.log("err = " + err);
-  console.log("data = " + JSON.stringify(data));
-  if(err) throw(err);
-  var doCall = make_callDescribeSpotInstanceRequests(data);
-  setTimeout(doCall, 1000);
-
-  var requestId = data.SpotInstanceRequests[0].SpotInstanceRequestId;
-  var params = {SpotInstanceRequestIds:[requestId]};
-  var callback = hDescribeSpotInstanceRequests;
-  ec2.describeSpotInstanceRequests(params, callback);
- };
- 
- function make_callDescribeSpotInstanceRequests(data) {
-  var requestId = data.SpotInstanceRequests[0].SpotInstanceRequestId;
-  var params = {SpotInstanceRequestIds:[requestId]};
-  function doCall() {
-   ec2.describeSpotInstanceRequests(params, handler);
+ try {
+  function hRequestSpotInstances(err, data) {
+   try {
+    console.log("requestSpotInstances:");
+    console.log("err = " + err);
+    console.log("data = " + JSON.stringify(data));
+    if(err) throw(err);
+    var doCall = make_callDescribeSpotInstanceRequests(data);
+    setTimeout(doCall, 1000);
+   } catch(ex3) {
+    emitter.emit('error', ex3);
+   }
   };
+ 
+  function make_callDescribeSpotInstanceRequests(data) {
+   try {
+    var requestId = data.SpotInstanceRequests[0].SpotInstanceRequestId;
+    var params = {SpotInstanceRequestIds:[requestId]};
+    function doCall() {
+     try {
+      ec2.describeSpotInstanceRequests(params, handler);
+     } catch(ex4) {
+      emitter.emit('error', ex4);
+     }
+    };
+    function handler(err, data) {
+     try {
+      console.log("describeSpotInstanceRequests:");
+      console.log("err = " + err);
+      console.log("data = " + JSON.stringify(data));
+      if(err) throw(err);
+      // check for states "open" and "active"
+      var state = data.SpotInstanceRequests[0].State;
+      if(state == "open") setTimeout(doCall, 1000);
+      else if(state == "active") {
+       var nextCall = make_callDescribeInstanceStatus(data);
+       nextCall();
+      }
+      else throw(err);
+     } catch(ex4) {
+      emitter.emit('error', ex4);
+     }
+    }
+   } catch(ex3) {
+    emitter.emit('error', ex3);
+   }
+   return(doCall);
+  };
+ 
+  function make_callDescribeInstanceStatus(data) {
+   try {
+    instanceId = data.SpotInstanceRequests[0].InstanceId;
+    var params = {InstanceIds:[instanceId]};
+    function doCall() {
+     try {
+      ec2.describeInstances(params, handler);
+     } catch(ex4) {
+      emitter.emit('error', ex4);
+     }
+    };
+    function handler(err, data) {
+     try {
+      console.log("describeInstances:");
+      console.log("err = " + err);
+      console.log("data = " + JSON.stringify(data));
+      var state = data.Reservations[0].Instances[0].State.Name;
+      console.log("state = " + state);
+      if(state == "running") {
+       var name = data.Reservations[0].Instances[0].PublicDnsName;
+       var address = data.Reservations[0].Instances[0].PublicIpAddress;
+       data.name = name;
+       data.address = address;
+       runCallback(err, data);
+      } else {
+       setTimeout(doCall, 2000);
+      }
+     } catch(ex4) {
+      emitter.emit('error', ex4);
+     }
+    };
+   } catch(ex3) {
+    emitter.emit('error', ex3);
+   }
+   return(doCall);
+  };
+ } catch(ex2) {
+  emitter.emit('error', ex2);
+ }
+ return(emitter);
+};
+
+function stop() {
+ if(instanceId) {
+  var params = {InstanceIds:[instanceId]};
+  ec2.stopInstances(params, handler);
   function handler(err, data) {
+   console.log("stopInstances:");
    console.log("err = " + err);
    console.log("data = " + JSON.stringify(data));
    if(err) throw(err);
-   // check for states "open" and "active"
-   var state = data.SpotInstanceRequests[0].State;
-   if(state == "open") setTimeout(doCall, 1000);
-   else if(state == "active") {
-    var nextCall = make_callDescribeInstanceStatus(data);
-    nextCall();
-   }
-   else throw(err);
-  }
-  return(doCall);
- };
- 
- function make_callDescribeInstanceStatus(data) {
-  var instanceId = data.SpotInstanceRequests[0].InstanceId;
-  var params = {InstanceIds:[instanceId]};
-  function handler(err, data) {
-   console.log("err = " + err);
-   console.log("data = " + JSON.stringify(data));
-   setTimeout(doCall, 2000);
   };
-  function doCall() {
-   //ec2.describeInstanceStatus(params, handler);
-   ec2.describeInstances(params, handler);
-  };
-  return(doCall);
- };
+ } else {
+  throw("stop: No running instances found");
+ }
 };
 
-exports.run = run;
+module.exports.run = run;
+module.exports.stop = stop;
