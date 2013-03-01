01fce36f81e46c98cc12b7f0cab4ea150c48e85e
Jason Kridner <jdk@ti.com>
2013-03-01 16:24:50 +0000

    ec2-build/build-kernel: switch to Linaro compiler

--- ec2-build/build-kernel.sh
+++ ec2-build/build-kernel.sh
@@ -3,6 +3,7 @@ set -e
 set -x
 PATH=/usr/local/angstrom/arm/bin:/mnt/u-boot/tools:$PATH
 BUILD=/mnt/build
+TOOLS=arm-linux-gnueabi-
 node --version
 git clone git://github.com/jadonk/kernel.git $BUILD/kernel
 git clone git://github.com/jadonk/u-boot.git $BUILD/u-boot
@@ -16,11 +17,11 @@ cp $BUILD/kernel/configs/beaglebone $BUILD/kernel/kernel/arch/arm/configs/beagle
 cp $BUILD/am33x-cm3/bin/am335x-pm-firmware.bin $BUILD/kernel/kernel/firmware/am335x-pm-firmware.bin
 mkdir -p $BUILD/kernel/kernel/rootfs
 cd $BUILD/kernel/kernel
-make ARCH=arm CROSS_COMPILE=arm-angstrom-linux-gnueabi- beaglebone_defconfig
-make ARCH=arm CROSS_COMPILE=arm-angstrom-linux-gnueabi- -j16 uImage dtbs
-make ARCH=arm CROSS_COMPILE=arm-angstrom-linux-gnueabi- -j16 modules
-make ARCH=arm CROSS_COMPILE=arm-angstrom-linux-gnueabi- INSTALL_MOD_PATH=$BUILD/kernel/kernel/rootfs modules_install
-make ARCH=arm CROSS_COMPILE=arm-angstrom-linux-gnueabi- uImage-dtb.am335x-bone
-make ARCH=arm CROSS_COMPILE=arm-angstrom-linux-gnueabi- uImage-dtb.am335x-bonelt
+make ARCH=arm CROSS_COMPILE=$TOOLS beaglebone_defconfig
+make ARCH=arm CROSS_COMPILE=$TOOLS -j16 uImage dtbs
+make ARCH=arm CROSS_COMPILE=$TOOLS -j16 modules
+make ARCH=arm CROSS_COMPILE=$TOOLS INSTALL_MOD_PATH=$BUILD/kernel/kernel/rootfs modules_install
+make ARCH=arm CROSS_COMPILE=$TOOLS uImage-dtb.am335x-bone
+make ARCH=arm CROSS_COMPILE=$TOOLS uImage-dtb.am335x-bonelt
 cd $BUILD/kernel/kernel/rootfs
 tar -cvzf modules.tgz lib
