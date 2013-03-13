3981b840dad1c20210ba71f6c61a2fe6ef7bcb9a
Jason Kridner <jdk@ti.com>
2013-03-13 07:39:10 +0000

    ec2-build: updated execution paths

--- ec2-build/server.sh
+++ ec2-build/server.sh
@@ -4,7 +4,7 @@ var connect = require('connect');
 var app = connect();
 app.use(connect.favicon());
 app.use(connect.logger('dev'));
-app.use(connect.directory(__dirname));
-app.use(connect.static(__dirname));
+app.use(connect.directory(process.cwd()));
+app.use(connect.static(process.cwd()));
 
 app.listen(8081);
