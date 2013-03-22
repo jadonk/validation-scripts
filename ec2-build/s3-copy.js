1cf8f141a1a93509c3e0574772b156c58ff5d7f2
Jason Kridner <jdk@ti.com>
2013-03-22 13:08:07 +0000

    s3-copy: walk directory tree

--- ec2-build/s3-copy.js
+++ ec2-build/s3-copy.js
@@ -1,29 +1,100 @@
 var AWS = require('aws-sdk');
+var fs = require('fs');
 
 var s3 = null;
 
 function copy_to_s3(config, source, bucket, dest, callback) {
- try {
-  AWS.config.update(config.client);
-  s3 = new AWS.S3.Client({region:'us-east-1'});
- } catch(ex2) {
-  emitter.emit('error', s3);
+ if(!s3) {
+  try {
+   AWS.config.update(config.client);
+   s3 = new AWS.S3.Client({region:'us-east-1'});
+  } catch(ex) {
+   throw 'Error ' + ex;
+  }
  }
 
-function dummy() {
-fs.readFile(sourceFile, function (err, data) {
-    if (err) { throw err; }
+ var pending = 0;
+ var stop = 0;
 
-    s3.client.putObject({
-        Bucket: bucketName,
-        Key: 'Folder/image.jpg',
-        Body: data,
-        ACL:'public-read'
-    }, function (res) {
-            console.log('Successfully uploaded file.');
-        })
+ dive(source);
 
-});
+ function dive(dir) {
+  if(stop) {
+   return; // if we are already dead, we don't do anything
+  }
+  pending++;
+  fs.readdir(dir, function(err, list) {
+   if(stop) {
+    return; // if we are already dead, we don't do anything
+   }
+   if (err) {
+     fail(err); // if an error occured, let's fail
+     return;
+   }
+   // iterate over the files
+   list.forEach(function(file) {
+    if(!dead) { // if we are already dead, we don't do anything
+     var path = dir + "/" + file;
+     pending++; // async operation starting after this line
+     fs.stat(path, function(err, stat) {
+      if(!dead) { // if we are already dead, we don't do anything
+       if (err) {
+        fail(err); // if an error occured, let's fail
+       } else {
+        if (stat && stat.isDirectory()) {
+         dive(path); // it's a directory, let's explore recursively
+        } else {
+         copy(path, stat); // it's not a directory, just perform the action
+        }
+        pending--; checkSuccess(); // async operation complete
+       }
+      }
+     });
+    }
+   });
+   pending--;
+   checkSuccess(); // async operations complete
+  });
+ }
+
+ function copy(file, stat) {
+  if(!stop) {
+   try {
+    var destFile = dest + file;
+    do_copy(file, destFile);
+   } catch(ex) {
+    fail('Copy failed on ' + file + ': ' + ex);
+   }
+ }
+
+ function do_copy(sourceFile, destFile) {
+  fs.readFile(sourceFile, do_write);
+  function do_write(err, data) {
+   if (err) { throw err; }
+
+   s3.client.putObject({
+    Bucket: bucket,
+    Key: destFile,
+    Body: data,
+    ACL: 'public-read'
+   }, function (res) {
+    winston.info('Successfully uploaded ' + sourceFile);
+   });
+  });
+ }
+
+ function checkSuccess() {
+  if(!stop && !pending) {
+   callback();
+  }
+ }
+
+ function fail(err) {
+  if(!stop) {
+   stop = true;
+   callback(err);
+  }
+ }
 }
 
 module.exports.copy_to_s3 = copy_to_s3;
