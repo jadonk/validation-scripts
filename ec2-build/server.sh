a529f2625f58f79d18bfd9b28fc86832225069d0
Jason Kridner <jdk@ti.com>
2013-03-23 17:56:15 +0000

    ec2-build/server.sh: attempted to fix s3copy service

--- ec2-build/server.sh
+++ ec2-build/server.sh
@@ -5,7 +5,8 @@ var s3copy = require('./s3-copy');
 var app = connect();
 app.use(connect.favicon());
 app.use(connect.logger('dev'));
-app.use(connect.post('/s3copy', s3CopyHandler));
+app.use(connect.bodyParser());
+app.use(s3CopyHandler);
 app.use(connect.directory(process.cwd()));
 app.use(connect.static(process.cwd()));
 
@@ -13,15 +14,15 @@ app.listen(8081);
 
 function s3CopyHandler(req, res) {
  if(
-  req.data["config"] &&
-  req.data["source"] &&
-  req.data["bucket"] &&
-  req.data["dest"]
+  req.body["config"] &&
+  req.body["source"] &&
+  req.body["bucket"] &&
+  req.body["dest"]
  ) {
-  var config = req.data["config"];
-  var source = req.data["source"];
-  var bucket = req.data["bucket"];
-  var dest = req.data["dest"];
+  var config = req.body["config"];
+  var source = req.body["source"];
+  var bucket = req.body["bucket"];
+  var dest = req.body["dest"];
 
   var now = new Date();
   var date = now.toJSON();
