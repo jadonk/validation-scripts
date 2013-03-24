7d1af98fa00bde4b80a5aeed174aeded027e2bc1
Jason Kridner <jdk@ti.com>
2013-03-24 21:16:43 +0000

    ec2-build: add s3copy.html

--- ec2-build/server.sh
+++ ec2-build/server.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 var express = require('express');
 var s3copy = require('./s3-copy');
+var io = require('socket.io');
 
 var app = express();
 app.use(express.favicon());
@@ -9,7 +10,23 @@ app.use(express.bodyParser());
 app.post('/s3copy', s3CopyHandler);
 app.use(express.directory(process.cwd()));
 app.use(express.static(process.cwd()));
+io.listen(app);
 app.listen(8081);
+
+io.sockets.on('connection', onConnection);
+function onConnection(socket) {
+ socket.on('s3copy', onS3copy);
+ function onS3copy(data) {
+  console.log('s3copy: ' + data);
+  s3copy.copy_to_s3(data.config, data.source, data.bucket, data.dest, oncomplete, onupdate);
+  function oncomplete(data) {
+   socket.emit('s3copy-complete', data);
+  }
+  function onupdate(data) {
+   socket.emit('s3copy-update', data);
+  }
+ }
+}
 
 function s3CopyHandler(req, res) {
  if(
