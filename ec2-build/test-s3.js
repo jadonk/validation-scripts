11c12bdcce8c09be2011efa68ef89846b2cdd7b7
Jason Kridner <jdk@ti.com>
2013-03-25 08:01:14 +0000

    ec2-build/test-s3.js: fixed quote

--- ec2-build/test-s3.js
+++ ec2-build/test-s3.js
@@ -6,7 +6,7 @@ var date = now.toJSON();
 
 s3copy.copy_to_s3(config, '/mnt/build/build.log', 'beagleboard', 'test-upload-' + date, oncomplete);
 s3copy.copy_to_s3(config, '/mnt/build/kernel/kernel/rootfs/modules.tgz', 'beagleboard', 'test-upload-' + date, oncomplete);
-s3copy.copy_to_s3(config, '/mnt/build/kernel/kernel/arch/arm/boot/uImage, 'beagleboard', 'test-upload-' + date, oncomplete);
+s3copy.copy_to_s3(config, '/mnt/build/kernel/kernel/arch/arm/boot/uImage', 'beagleboard', 'test-upload-' + date, oncomplete);
 s3copy.copy_to_s3(config, '/mnt/build/kernel/kernel/arch/arm/boot/uImage-dtb.am335x-bone', 'beagleboard', 'test-upload-' + date, oncomplete);
 s3copy.copy_to_s3(config, '/mnt/build/kernel/kernel/arch/arm/boot/uImage-dtb.am335x-boneblack', 'beagleboard', 'test-upload-' + date, oncomplete);
 s3copy.copy_to_s3(config, '/mnt/build/kernel/kernel/arch/arm/boot/dts/dtb.tgz', 'beagleboard', 'test-upload-' + date, oncomplete);
