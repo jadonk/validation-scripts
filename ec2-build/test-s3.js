fa54fb45da704351db410b54b7a5c14c05554d2a
Jason Kridner <jdk@ti.com>
2013-03-25 16:02:48 -0400

    ec2-build: update stuff to be saved

--- ec2-build/test-s3.js
+++ ec2-build/test-s3.js
@@ -11,7 +11,9 @@ var date = now.toJSON();
 //s3copy.copy_to_s3(config, '/mnt/build/kernel/kernel/arch/arm/boot/uImage-dtb.am335x-boneblack', 'beagleboard', 'test-upload-' + date, oncomplete);
 //s3copy.copy_to_s3(config, '/mnt/build/kernel/kernel/arch/arm/boot/dts/dtb.tgz', 'beagleboard', 'test-upload-' + date, oncomplete);
 s3copy.copy_to_s3(config, '/mnt/build/build.log', 'beagleboard', 'angstrom-' + date, oncomplete);
-s3copy.copy_to_s3(config, '/mnt/build/oe/build', 'beagleboard', 'angstrom-' + date, oncomplete);
+s3copy.copy_to_s3(config, '/mnt/build/oe/build/tmp-angstrom_v2012_12-eglibc/deploy/images/beaglebone', 'beagleboard', 'angstrom-' + date, oncomplete);
+s3copy.copy_to_s3(config, '/mnt/build/oe/build/output.tgz', 'beagleboard', 'angstrom-' + date, oncomplete);
+s3copy.copy_to_s3(config, '/mnt/build/oe/build/sstate-cache.tgz', 'beagleboard', 'angstrom-' + date, oncomplete);
 
 function oncomplete(err) {
  if(err) console.log("Error: " + err);
