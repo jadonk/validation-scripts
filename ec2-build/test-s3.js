4a6db982eeb8984538edd9f8f235e73c90bceb6a
Jason Kridner <jdk@ti.com>
2013-03-25 16:10:00 +0000

    ec2-build: save off the build log

--- ec2-build/test-s3.js
+++ ec2-build/test-s3.js
@@ -10,7 +10,8 @@ var date = now.toJSON();
 //s3copy.copy_to_s3(config, '/mnt/build/kernel/kernel/arch/arm/boot/uImage-dtb.am335x-bone', 'beagleboard', 'test-upload-' + date, oncomplete);
 //s3copy.copy_to_s3(config, '/mnt/build/kernel/kernel/arch/arm/boot/uImage-dtb.am335x-boneblack', 'beagleboard', 'test-upload-' + date, oncomplete);
 //s3copy.copy_to_s3(config, '/mnt/build/kernel/kernel/arch/arm/boot/dts/dtb.tgz', 'beagleboard', 'test-upload-' + date, oncomplete);
-s3copy.copy_to_s3(config, '/mnt/build/oe/build', 'beagleboard', 'test-upload-' + date, oncomplete);
+s3copy.copy_to_s3(config, '/mnt/build/build.log', 'beagleboard', 'angstrom-' + date, oncomplete);
+s3copy.copy_to_s3(config, '/mnt/build/oe/build', 'beagleboard', 'angstrom-' + date, oncomplete);
 
 function oncomplete(err) {
  if(err) console.log("Error: " + err);
