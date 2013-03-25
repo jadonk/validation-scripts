fc11c40345622a9e591c403bb90427087cce293b
Jason Kridner <jdk@ti.com>
2013-03-25 01:52:02 +0000

    ec2-build/test-s3.js: change directory to copy

--- ec2-build/test-s3.js
+++ ec2-build/test-s3.js
@@ -4,7 +4,7 @@ var config = require('config');
 var now = new Date();
 var date = now.toJSON();
 
-s3copy.copy_to_s3(config, 'test', 'beagleboard', 'test-upload-' + date, oncomplete);
+s3copy.copy_to_s3(config, '/mnt/build', 'beagleboard', 'test-upload-' + date, oncomplete);
 function oncomplete(err) {
  if(err) console.log("Error: " + err);
  console.log("Completed");
