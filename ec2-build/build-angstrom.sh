cf554e3e4c1ba2c247f3c8c9329a11ae72bbc372
Jason Kridner <jdk@ti.com>
2013-03-28 10:05:15 -0400

    ec2-build/build-angstrom.sh: update and build for rebuilds

--- ec2-build/build-angstrom.sh
+++ ec2-build/build-angstrom.sh
@@ -6,14 +6,20 @@ BRANCH=angstrom-v2012.12-yocto1.3
 DATE=`date +%F-%T`
 #TAG=f6fe4ce6f4f82be5b2185b12ccadb163a61e512e
 date
-#time git clone git://github.com/jadonk/setup-scripts.git /mnt/build/oe
-time git clone git://github.com/Angstrom-distribution/setup-scripts.git /mnt/build/oe
-cd $BUILD/oe
-time git checkout -b $BRANCH origin/$BRANCH
-time MACHINE=beaglebone ./oebb.sh config beaglebone
-sed -i 's/^PARALLEL_MAKE.*$/PARALLEL_MAKE = "-j4"/' $BUILD/oe/conf/local.conf
-sed -i 's/^BB_NUMBER_THREADS.*$/BB_NUMBER_THREADS = "3"/' $BUILD/oe/conf/local.conf
-#time MACHINE=beaglebone ./oebb.sh update commit $TAG
+if [ -e $BUILD/oe ]
+ cd $BUILD
+# time MACHINE=beaglebone ./oebb.sh update commit $TAG
+ time MACHINE=beaglebone ./oebb.sh update
+else
+# time git clone git://github.com/jadonk/setup-scripts.git /mnt/build/oe
+ time git clone git://github.com/Angstrom-distribution/setup-scripts.git /mnt/build/oe
+ cd $BUILD/oe
+ time git checkout -b $BRANCH origin/$BRANCH
+ time MACHINE=beaglebone ./oebb.sh config beaglebone
+ sed -i 's/^PARALLEL_MAKE.*$/PARALLEL_MAKE = "-j4"/' $BUILD/oe/conf/local.conf
+ sed -i 's/^BB_NUMBER_THREADS.*$/BB_NUMBER_THREADS = "3"/' $BUILD/oe/conf/local.conf
+# time MACHINE=beaglebone ./oebb.sh update commit $TAG
+fi
 source ~/.oe/environment-angstromv2012.12
 #time bitbake -k console-image
 time bitbake -k cloud9-gnome-image
