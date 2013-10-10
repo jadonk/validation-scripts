a0904906c966e9a8e8e92d3695e14976a94ec100
Jason Kridner <jdk@ti.com>
2013-10-10 22:53:15 +0000

    build-buildroot.sh: updated e-mail notification message

--- ec2-build/build-buildroot.sh
+++ ec2-build/build-buildroot.sh
@@ -31,6 +31,6 @@ node -pe "c=require('$HOME/config');require('fs').readFileSync('$BUILD/ec2-build
 s3cmd sync -P $OUTPUT/ s3://beagle/buildroot/$DATE/
 s3cmd put -P $BUILD/build.log s3://beagle/buildroot/$DATE/
 
-node -pe "c=require('$HOME/config');a=require('aws-sdk');a.config.update(c.client);s=new a.SES.Client(c.client);s.sendEmail({Source:c.email.from,Destination:{ToAddresses:c.email.to},Message:{Subject:{Data:'Completed build-buildroot.sh'},Body:{Text:{Data:'http://beagle.s3.amazonaws.com/buildroot/$DATE/index.html'}}}},function(err){console.log('err = ' + err);});"
+node -pe "c=require('$HOME/config');a=require('aws-sdk');a.config.update(c.client);s=new a.SES.Client(c.client);s.sendEmail({Source:c.email.from,Destination:{ToAddresses:c.email.to},Message:{Subject:{Data:'[beagle-validation] Completed build-buildroot.sh'},Body:{Text:{Data:'http://beagle.s3.amazonaws.com/buildroot/$DATE/index.html\nhttp://github.com/jadonk/validation-scripts/ec2-build'}}}},function(err){console.log('err = ' + err);});"
 
 echo !!!! COMPLETED build-buildroot !!!!
